daf726611fed3e44b7b26b2c55506297005b17c2
Razvan Turcanu <razvan.turcanu@visma.com>
2026-06-09 23:44:17 +0300

    update nav

--- app/data/content.ts
+++ app/data/content.ts
@@ -2,10 +2,10 @@
 export type NavLink = { label: string; href: string };
 
 export const navLinks: NavLink[] = [
-  { label: "Services", href: "/services" },
+  // { label: "Services", href: "/services" },
   { label: "Portfolio", href: "/portfolio" },
-  { label: "About Me", href: "/" },
-  { label: "Blog", href: "/blog" },
+  { label: "About", href: "/" },
+  // { label: "Blog", href: "/blog" },
 ];
 
 export const contactLink: NavLink = { label: "Contact", href: "/contact" };
